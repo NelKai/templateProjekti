868c5b1a4e254abb5e0cce0a5aa4ed8a4e1b7713
NelKai <55228995+NelKai@users.noreply.github.com>
2021-03-18 16:18:26 +0200

    Projektipäivitys

--- webserver.js
+++ webserver.js
@@ -3,37 +3,6 @@ var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
 
-/*
-var request = require("request");
-var http = require("http");
-var data;
-
-request(
-        "https://test.spaceflightnewsapi.net/api/v2/articles",
-        { json: true },
-        (err, res, body) => {
-            if (err) {
-                return console.log(err);
-            }
-            
-            console.log("Haetaan tietoja...");
-            
-            data = body;
-            
-console.log(data);
-
-
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i].title);
-            }
-
-            
-
-        }
-    );
-
-*/
-
 app.get("/", function(request, response) {
 
     response.render("pages/index");
